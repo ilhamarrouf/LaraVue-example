2684ba34a99a9acec6cd7cc32cfa46a6ef62aab7
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2017-09-07 13:28:22 +0700

    database seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,14 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    $user = \App\User::inRandomOrder()->first();
+
+    return [
+        'user_id' => $user->id,
+        'title' => $faker->sentence,
+        'body' => $faker->text,
+    ];
+});
